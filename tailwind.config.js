4c1b566a38f4bbd5c3e12897b8c2a64f6c22a3df
phillip <phillipkilo6@gmail.com>
2025-11-02 17:49:19 -0800

    ✅ Fix Tailwind build config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 export default {
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
-  theme:{extend:{colors:{aura:'#a855f7'}, boxShadow:{aura:'0 0 30px #a855f7'}}},
-  plugins:[]
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: {} },
+  plugins: [],
 }
